4cc898e74b945612cfc7d95b875384cbad7b0ce5
ashish <ashishtyl@gmail.com>
2013-08-14 22:37:32 +0800

    hello world

--- Test-ejb/src/java/Test1.java
+++ Test-ejb/src/java/Test1.java
@@ -12,7 +12,7 @@ public class Test1 {
     public static void main (String args[])
             
     {
-        System.out.println("HI");
+        System.out.println("Hello Wolrd!");
     }
     
 }
