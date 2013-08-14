fc2c306b2cb63bea0867d45c3adfd5d402149264
user <user@user-PC>
2013-08-14 22:25:06 +0800

    Testing

---
+++ Test-ejb/src/java/Test1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Test1 {
+    
+    public static void main (String args[])
+            
+    {
+        System.out.println("HI");
+    }
+    
+}
